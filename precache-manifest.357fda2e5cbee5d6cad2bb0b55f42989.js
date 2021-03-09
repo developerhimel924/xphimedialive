44e109325cb292106f3089bea9dd9a45718a183d
Developer Himel <programmerhimel@gmail.com>
2021-03-09 21:42:14 +0600

    Updates

---
+++ precache-manifest.357fda2e5cbee5d6cad2bb0b55f42989.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e3dfab0ad32d33920b4e112f3563cee",
+    "url": "/xphimedialive/index.html"
+  },
+  {
+    "revision": "f90afac29d73f34b9c94",
+    "url": "/xphimedialive/static/css/main.8958c6a7.chunk.css"
+  },
+  {
+    "revision": "2350db96b8f53e4dd57a",
+    "url": "/xphimedialive/static/js/2.7f808649.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/xphimedialive/static/js/2.7f808649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90afac29d73f34b9c94",
+    "url": "/xphimedialive/static/js/main.735d8e47.chunk.js"
+  },
+  {
+    "revision": "e17c4889c12485beed90",
+    "url": "/xphimedialive/static/js/runtime-main.c774efaa.js"
+  },
+  {
+    "revision": "9a4e13144e0f8bb6257e08b6354257a9",
+    "url": "/xphimedialive/static/media/Animations-banner.9a4e1314.jpg"
+  },
+  {
+    "revision": "1be24a92e76b3000dd0b8840a9b1847d",
+    "url": "/xphimedialive/static/media/Movies-banner.1be24a92.jpg"
+  },
+  {
+    "revision": "c0fa1ba4f0e0c3824498c94b2c31fcb5",
+    "url": "/xphimedialive/static/media/ancient.c0fa1ba4.jpg"
+  },
+  {
+    "revision": "d42d11a56fefa388b97ae64b8b486564",
+    "url": "/xphimedialive/static/media/award.d42d11a5.jpg"
+  },
+  {
+    "revision": "4549dd77598abf1c5cf00578abada233",
+    "url": "/xphimedialive/static/media/book-banner.4549dd77.jpg"
+  },
+  {
+    "revision": "4dc3a0e13c501c0f3a8bd2d781e89d05",
+    "url": "/xphimedialive/static/media/evoking.4dc3a0e1.jpg"
+  },
+  {
+    "revision": "dd42dcbdbe13326edde86d77caaa3858",
+    "url": "/xphimedialive/static/media/hidden.dd42dcbd.jpg"
+  },
+  {
+    "revision": "2cb0150c4b1586c3eaa2c5ece1efa1f4",
+    "url": "/xphimedialive/static/media/image1.2cb0150c.jpg"
+  },
+  {
+    "revision": "2f1a1c7cd040c1a2501cdad4f9b1de6a",
+    "url": "/xphimedialive/static/media/indigenous.2f1a1c7c.jpg"
+  },
+  {
+    "revision": "9023c6e0d7c31ab592b6e7b1ee8d7c91",
+    "url": "/xphimedialive/static/media/rare.9023c6e0.jpg"
+  },
+  {
+    "revision": "07e517a842f6cd11e8b1caea70e3b8d1",
+    "url": "/xphimedialive/static/media/sublime.07e517a8.jpg"
+  }
+]);
